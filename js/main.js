83891f808584b4121f7c0bfeb7ff2c7646c591e9
NightVizor <60451285+NightVizor@users.noreply.github.com>
2020-09-23 23:52:29 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$('.laptop__screen').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    infinite: true,
+    dots: false,
+    speed: 1000,
+    arrows: false,
+    fade: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next')
+});
